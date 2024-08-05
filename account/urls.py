784ed42ee7a384ff63d48740e5816fe3618f547a
MuhammadAnwarBadat <anwarbadat1997.ab@gmail.com>
2024-08-05 17:13:40 +0500

    Added authetication on user login,and created dashboard page

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,17 @@
+# from django.urls import path
+
+# urlpatterns = [
+#     # Post views
+#     path('login/', views.user_login, name='login'),
+# ]
+
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # Post views
     path('login/', views.user_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout-then-login/', views.logout_then_login, name='logout_then_login'),
+    path('', views.dashboard, name='dashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
+
+def logout_then_login(request):
+    logout(request)
+    return redirect('login')
 
 def user_login(request):
     if request.method == 'POST':
@@ -21,3 +26,11 @@ def user_login(request):
         form = LoginForm()
     
     return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(
+        request,
+        'account/dashboard.html',
+        {'section': 'dashboard'}
+    )
